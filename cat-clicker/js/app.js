3c5feba82bfe5b0860d49022bb65965c312079cc
Catalin Stanciu <catacsdev@gmail.com>
2016-09-28 17:06:40 +0200

    Added 2 cats images with clicker

--- cat-clicker/js/app.js
+++ cat-clicker/js/app.js
@@ -1,13 +1,48 @@
-var counter = 0;
+var counter = {};
+var cats = [
+	{
+		id: 'cat1',
+		name: 'Cat1',
+		src: 'img/cat-1.jpg',
+	}, {
+		id:  'cat2',
+		name: 'Cat 2',
+		src: 'img/cat-2.jpg',
+	},
+];
 function app() {
-	var element = document.getElementById('cat-clicker');
-	element.addEventListener('click', handleCatClick, false);
+	var catsContainer = document.getElementById('cats-container');
+	for (var i = 0; i < cats.length; i++ ) {
+		var cat = cats[i];
+		counter[cat.id] = 0;
+		var catContainer = document.createElement("div");
+
+		var catImage = document.createElement("img");
+		catImage.id = cat.id;
+		catImage.src = cat.src;
+		catImage.addEventListener('click', handleCatClick, false);
+
+		var catLabel = document.createElement('p');
+		catLabel.textContent = cat.name;
+
+		var catCounter = document.createElement('p');
+		catCounter.textContent = counter[cat.id];
+		catCounter.id = cat.id + '-counter';
+
+		catContainer.appendChild(catImage);
+		catContainer.appendChild(catLabel);
+		catContainer.appendChild(catCounter);
+		catsContainer.appendChild(catContainer);
+		
+	}
 }
 
-function handleCatClick() {
-	var element = document.getElementById('click-counter');
-	counter++;
-	element.textContent = counter;
+function handleCatClick(event) {
+	var element = document.getElementById(event.target.id + '-counter');
+	var concreteCounter = counter[event.target.id];
+	concreteCounter++;
+	element.textContent = concreteCounter;
+	counter[event.target.id] = concreteCounter;
 }
 
 window.onload = function() {
